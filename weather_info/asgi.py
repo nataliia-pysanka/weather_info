86cc8ee25998689c698843f0e322d70784fad0ee
nataliia-pysanka <pysankanataliia@gmail.com>
2022-12-23 21:57:00 +0200

    start

--- weather_info/asgi.py
+++ weather_info/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for CompassWay project.
+ASGI config for weather_info project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CompassWay.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weather_info.settings')
 
 application = get_asgi_application()
--- weather_info/wsgi.py
+++ weather_info/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for CompassWay project.
+WSGI config for weather_info project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CompassWay.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weather_info.settings')
 
 application = get_wsgi_application()
